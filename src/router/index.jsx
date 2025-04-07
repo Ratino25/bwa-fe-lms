ffcbf04923f3c8f776c987707233b344e7236e00
Ratino <ratinoray@gmail.com>
2025-04-07 19:23:46 +0700

    Feat : Slicing Overview

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -3,6 +3,7 @@ import ManagerHome from "../pages/ManagerHome";
 import SignInPage from "../pages/SignIn";
 import SignUpPage from "../pages/SignUp";
 import SuccessCheckoutPage from "../pages/SuccessCheckout";
+import LayoutDashboard from "../components/layout";
 
 
 
@@ -22,6 +23,16 @@ const router = createBrowserRouter([
     {
       path: "/success-checkout",
       element: <SuccessCheckoutPage/>
+    },
+    {
+      path: "/manager",
+      element: <LayoutDashboard/>,
+      children: [
+        {
+          index: true,
+          element: <ManagerHome/>
+        }
+      ]
     }
   ]);
 
