b5ddba211bd88c195f0eac7f4f333402b6d0955f
Ratino <ratinoray@gmail.com>
2025-05-08 20:00:36 +0700

    front End done

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -5,15 +5,16 @@ import Courses from "../pages/Manager/home/courses";
 import Students from "../pages/Manager/home/students";
 import { Outlet, useMatch } from "react-router-dom";
 
-export default function LayoutDashboard({isAdmin = true}) {
-    const isPreviewPage = useMatch('/manager/courses/:id/preview')
+export default function LayoutDashboard({ isAdmin = true }) {
+    const isManagerPreviewPage = useMatch('/manager/courses/:id/preview')
+    const isStudentPreviewPage = useMatch("/student/detail-course/:id")
     return (
         <>
-            {isPreviewPage !== null ? (
+            {isManagerPreviewPage !== null || isStudentPreviewPage !== null ? (
                 <Outlet />
             ) : (
                 <div className="flex min-h-screen">
-                    <Sidebar isAdmin = {isAdmin} />
+                    <Sidebar isAdmin={isAdmin} />
                     <main className="flex flex-col flex-1 gap-[30px] p-[30px] ml-[290px]">
                         <Header />
                         <Outlet />
