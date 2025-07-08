8dd53426340d4b9a08660af1d50ed0ec410c0096
Ratino <ratinoray@gmail.com>
2025-07-08 20:39:35 +0700

    fix error icon course preview

--- src/pages/Manager/course-preview/index.jsx
+++ src/pages/Manager/course-preview/index.jsx
@@ -25,7 +25,7 @@ export default function ManageCoursePreviewPage() {
                                 <li key={item._id}>
                                 <button type="button" className="w-full text-left">
                                     <div className="flex items-center gap-3 w-full rounded-full border p-[14px_20px] transition-all duration-300 hover:bg-[#662FFF] hover:border-[#8661EE] hover:shadow-[-10px_-6px_10px_0_#7F33FF_inset] bg-[#070B24] border-[#24283E] shadow-[-10px_-6px_10px_0_#181A35_inset]">
-                                        <img src={`/assets/images/icons/${item.type === "text" ? 'note-white' : 'video-play-white.svg'}`} className="w-6 h-6" alt="icon" />
+                                        <img src={`/assets/images/icons/${item.type === "text" ? 'note-white' : 'video-play-white'}.svg`} className="w-6 h-6" alt="icon" />
                                         <span className="w-full font-semibold text-white line-clamp-1 transition-all duration-300 hover:line-clamp-none"> {item.title} </span>
                                     </div>
                                 </button>
