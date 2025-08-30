fbbc2729234b9ce39c326f0b756f3fcab9040669
Ratino <ratinoray@gmail.com>
2025-08-30 22:28:22 +0700

    Course 18 delete student

--- src/pages/Manager/students/student-item.jsx
+++ src/pages/Manager/students/student-item.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useRevalidator } from "react-router-dom";
 import PropTypes from "prop-types";
+import { deleteStudent } from "../../../services/studentService";
+import { useMutation } from "@tanstack/react-query";
 
 
 export default function StudentItem({
@@ -9,6 +11,21 @@ export default function StudentItem({
     totalCourse = 183,
     id = "1"
 }) {
+
+    const revalidator = useRevalidator();
+
+    const {isLoading, mutateAsync} = useMutation({
+        mutationFn: () => deleteStudent(id)
+    })
+
+    const handleDelete = async () => {
+        try {
+            await mutateAsync()
+            revalidator.revalidate()
+        } catch (error) {
+            console.log(error);
+        }
+    }
     return (
         <div className="card flex items-center gap-5">
             <div className="relative flex shrink-0 w-20 h-20">
@@ -29,7 +46,7 @@ export default function StudentItem({
                 <Link to={`/manager/students/edit/${id}`} className="w-fit rounded-full border border-[#060A23] p-[14px_20px] font-semibold text-nowrap">
                     Edit Profile
                 </Link>
-                <button type="button" className="w-fit rounded-full p-[14px_20px] bg-[#FF435A] font-semibold text-white text-nowrap">Delete</button>
+                <button type="button" disabled={isLoading} onClick={handleDelete} className="w-fit rounded-full p-[14px_20px] bg-[#FF435A] font-semibold text-white text-nowrap">Delete</button>
             </div>
         </div>
     )
